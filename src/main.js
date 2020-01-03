8f61f6187db4570b9986f31b7b588c6e5fd51311
Fmk1998 <1092911991@qq.com>
2020-01-03 10:00:33 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -2,7 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import Axios from 'axios';
 
+import VueAxios from 'vue-axios'
+
+Vue.use(ElementUI);
+Vue.use(VueAxios, axios)
+Vue.prototype.$axios=Axios; //设置原型
 Vue.config.productionTip = false
 
 new Vue({
