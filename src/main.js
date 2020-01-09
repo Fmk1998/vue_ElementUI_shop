d31b9059633325751565678198d9dae2d5995deb
Fmk1998 <1092911991@qq.com>
2020-01-09 14:26:54 +0800

    login

--- src/main.js
+++ src/main.js
@@ -2,16 +2,20 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
-import Axios from 'axios';
-
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import axios from 'axios'
 import VueAxios from 'vue-axios'
+//导入全局样式表
+import './assets/css/global.css'
+import './assets/iconfont/iconfont.css'
 
-Vue.use(ElementUI);
+Vue.use(ElementUI)
 Vue.use(VueAxios, axios)
-Vue.prototype.$axios=Axios; //设置原型
 Vue.config.productionTip = false
+
+
+Vue.prototype.$axios=axios;
 
 new Vue({
   router,
---
+++ vue.config.js
@@ -0,0 +1,42 @@
+module.exports = {
+
+  outputDir: 'dist',   //build输出目录
+
+  assetsDir: 'assets', //静态资源目录（js, css, img）
+
+  lintOnSave: false, //是否开启eslint
+
+  devServer: {
+
+    open: true, //是否自动弹出浏览器页面
+
+    host: "localhost",
+
+    port: '8080',
+
+    https: false,
+
+    hotOnly: false,
+
+    proxy: {
+
+      '/api': {
+
+        // target: 'https://www.liulongbin.top:8888/api/private/v1/', //外部服务器的地址
+        target: 'http://127.0.0.1:8089/study-boot', //自己的服务器的地址
+
+        changeOrigin: true,
+
+        pathRewrite: {
+
+          '^/api': ''
+
+        }
+
+      }
+
+    }
+
+  }
+
+}
